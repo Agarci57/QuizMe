3f016643b881a1474e7b67ae7f641c67d76e3999
Adrian Garcia <adgarcia92@hotmail.com>
2016-01-25 18:46:51 -0600

    Changed parent layout and minor changes to content_quiz

--- app/src/main/java/com/quizme/garbytes/quizme/QuizActivity.java
+++ app/src/main/java/com/quizme/garbytes/quizme/QuizActivity.java
@@ -8,8 +8,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
+
+    private Button mSubmitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,14 +21,14 @@ public class QuizActivity extends AppCompatActivity {
         setContentView(R.layout.activity_quiz);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        mSubmitButton = (Button)findViewById(R.id.submit_button);
+        mSubmitButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
+            public void onClick(View v) {
+                // This is an anonymous inner class
+                // It will take actions when button is pressed
+                Toast.makeText(QuizActivity.this, "Correct! err maybe.. idk.", Toast.LENGTH_LONG);
+        }
         });
     }
 
